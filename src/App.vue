70b82cc9be6beb792bc9e39cef4577a860c13595
olga_demina <olga.demina1410@gmail.com>
2024-05-06 12:49:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+import CardsList from "./components/CardsList.vue";
+
+export default {
+  components: {
+    AppHeader,
+    CardsList,
+  },
+  data() {
+    return {
+      cardsArray: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php", {
+        params: {
+          num: 20,
+          offset: 0,
+        },
+      })
+      .then((resp) => {
+        console.log(resp);
+        this.cardsArray = resp.data.data;
+      });
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <CardsList :cardsArray="cardsArray" />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    cardObj: Object,
+  },
+  computed: {
+    image() {
+      return this.cardObj.card_images[0].image_url_small;
+    },
+    archetype() {
+      return this.cardObj.archetype
+        ? this.cardObj.archetype
+        : "nessun archetipo";
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="image" alt="" />
+    <div class="card-body">
+      <h4>{{ cardObj.name }}</h4>
+      <p>{{ archetype }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
